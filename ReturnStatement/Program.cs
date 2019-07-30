b07b167a6167011ff994098a3f2b676051bb3b5a
anthonyalchemy <anthony@alchemytechs.com>
2019-07-30 16:16:37 +0100

    Add project files.

---
+++ ReturnStatement/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ReturnStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int cubedNumber = cube(5); this is another way of returning from the one below
+            //Console.WriteLine(cubedNumber);
+
+
+            Console.WriteLine(cube(5));
+
+
+            Console.ReadLine();
+        }
+
+
+        static int cube(int num)//num will be number user passes in 
+        {
+            int result = num * num * num;
+            return result;//returns result to caller
+
+        }
+    }
+}
